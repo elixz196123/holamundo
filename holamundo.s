6bfa08841359baa23ced52acf4fc35c51ac8e7f5
elixz196123 <71353800+elixz196123@users.noreply.github.com>
2020-12-15 22:59:18 -0800

    Update holamundo.s

--- holamundo.s
+++ holamundo.s
@@ -1,7 +1,23 @@
-org 100h
-mov msg[2], 34H
-mov dx, offset msg 
-mov ah, 9
-int 21h
-ret
-msg db "Hola mundo estoy corriendo un programa en ensamblador $"
+.data
+
+/* Data segment: define our message string and calculate its length. */
+msg:
+    .ascii        "Hello, ARM64!\n"
+len = . - msg
+
+.text
+
+/* Our application's entry point. */
+.globl _start
+_start:
+    /* syscall write(int fd, const void *buf, size_t count) */
+    mov     x0, #1      /* fd := STDOUT_FILENO */
+    ldr     x1, =msg    /* buf := msg */
+    ldr     x2, =len    /* count := len */
+    mov     w8, #64     /* write is syscall #64 */
+    svc     #0          /* invoke syscall */
+
+    /* syscall exit(int status) */
+    mov     x0, #0      /* status := 0 */
+    mov     w8, #93     /* exit is syscall #1 */
+    svc     #0          /* invoke syscall */
